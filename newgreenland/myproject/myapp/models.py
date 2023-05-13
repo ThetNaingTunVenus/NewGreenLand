e36d00a74e4b66a21d3303236efb6dee8fc307fb
kothet <kothetnaingtun.ict@gmail.com>
2023-05-13 06:48:22 +0630

    add templates dir

--- newgreenland/myproject/myapp/models.py
+++ newgreenland/myproject/myapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Buyer(models.Model):
+    id = models.AutoField(primary_key=True)
+    BuyerName = models.CharField(max_length=225)
+    Address = models.CharField(max_length=225)
+    def __str__(self):
+        return self.BuyerName
+
+class Style(models.Model):
+    id = models.AutoField(primary_key=True)
+    BuyerName = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    StyleCode = models.CharField(max_length=225,unique=True)
+    ItemName = models.CharField(max_length=225,blank=True,null=True)
+    Buyer = models.CharField(max_length=225)
+    def __str__(self):
+        return self.StyleCode
--- newgreenland/myproject/myapp/views.py
+++ newgreenland/myproject/myapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.db.models import Sum,Count,F
+from django.http import HttpResponse
+from django.views.generic import TemplateView, View, CreateView, DetailView,FormView
+from django.urls import reverse_lazy
+
+from django.core.paginator import Paginator
+
+from .forms import *
+from .models import *
+
+class Test(View):
+    def get(self,request):
+        return render(request, 'base.html')
